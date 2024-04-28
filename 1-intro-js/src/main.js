fcdeaed50b0c728a3420b85808720883f4af7244
MiguelAEG <miguel.egk@gmail.com>
2024-04-27 21:17:06 -0600

    Objetos Relacionados

--- 1-intro-js/src/main.js
+++
@@ -1,9 +0,0 @@
-function obtenerinfo(name= 'Miguel', apellido= 'Enriquez'){
-    const info = `${name} ${apellido}`;
-
-    return info;
-}
-
-const info = obtenerinfo('Angel', 'Garcia');
-
-console.log(info);
