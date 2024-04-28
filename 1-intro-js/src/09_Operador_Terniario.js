ac03c4feed2d30166be3c1d96952d87a44580ceb
MiguelAEG <miguel.egk@gmail.com>
2024-04-27 21:28:43 -0600

    Operador Terniario

---
+++ 1-intro-js/src/09_Operador_Terniario.js
@@ -0,0 +1,18 @@
+const promedio = 9.0;
+
+let resultado = '';
+
+resultado = (promedio >= 7.0) ? "Aprobado" : "Reprobado";
+
+console.log(resultado)
+
+let valorMaximo = 0;
+
+const a = 4;
+const b = 6;
+const c = 3;
+
+valorMaximo = a > b ? a : b;
+valorMaximo = valorMaximo > c ? valorMaximo : c;
+
+console.log(valorMaximo);
